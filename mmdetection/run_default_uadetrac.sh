a63ff07ea6a2369072af68cda81b837b4cb97f07
2524463910@qq.com <lizhaoliu@tencent.com>
2021-10-01 00:07:34 +0800

    new features: add conformer and wide conformer related config and scripts for uadetrac dataset

---
+++ mmdetection/run_default_uadetrac.sh
@@ -0,0 +1,15 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export OMP_NUM_THREADS=1
+GPU_NUM=8
+
+CONFIG="./configs/faster_rcnn/faster_rcnn_conformer_small_patch32_fpn_1x_uadetrac.py"
+WORK_DIR='./work_dir/faster_rcnn_conformer_small_patch32_lr_1e_4_fpn_1x_uadetrac_1344_800'
+
+# Train
+python -m torch.distributed.launch --nproc_per_node=${GPU_NUM} --master_port=50040 --use_env ./tools/train.py ${CONFIG} --work-dir ${WORK_DIR} --gpus ${GPU_NUM}  --launcher pytorch --cfg-options model.pretrained='./pretrain_models/Conformer_small_patch32.pth' model.backbone.patch_size=32
+
+# Test on multiple cards
+python -m torch.distributed.launch --nproc_per_node=${GPU_NUM} --master_port=50040 --use_env ./tools/test.py ${CONFIG} ${WORK_DIR}/latest.pth --launcher pytorch  --eval bbox
+
+# Test on single card
+#./tools/test.py ${CONFIG} ${WORK_DIR}/latest.pth --eval bbox
